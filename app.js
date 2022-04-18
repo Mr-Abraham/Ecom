8466045b0242d22928cdf4eaa4a449c40aa9df58
Shivam Abraham <codingnerd84@gmail.com>
2022-04-18 09:30:09 +0530

    added the prfile page and designed the profile nav along with the login and signup navbar

--- app.js
+++ app.js
@@ -5,15 +5,16 @@ const dropdowns = document.querySelectorAll('.dropdown');
 const menubtn = document.querySelector('.menubtn');
 dropdowns.forEach((dropdown)=>{
     dropdown.addEventListener('click',(e)=>{
-        console.log('drop down clicked')
-
         const dropparent = e.currentTarget.parentElement;
         dropparent.classList.toggle('active-drop')
     })
 })
-
 menubtn.addEventListener('click',function(){
 cateogaries.classList.toggle('active-cateogary');
 menubtn.classList.toggle('active-menu');
-
+})
+const userDrop = document.querySelector('.userDrop');
+const logutdropdown = document.querySelector('.logout-dropdown');
+userDrop.addEventListener('click',function(){
+    logutdropdown.classList.toggle('active-logout-dropdown')
 })
